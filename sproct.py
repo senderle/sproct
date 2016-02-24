05e8b5588f6b3d4dfa3fcfe8ad040330249deba1
Scott Enderle <scott.enderle@gmail.com>
2016-02-24 16:39:52 -0500

    add bag of bigrams similarity test

--- sproct.py
+++ sproct.py
@@ -1,8 +1,8 @@
 import re
 import argparse
 from functools import wraps
-from collections import namedtuple, defaultdict, Hashable
-from difflib import SequenceMatcher
+from collections import namedtuple, defaultdict, Hashable, Counter
+from difflib import SequenceMatcher, Differ
 
 _word_regex = re.compile(r'\w+')
 _word_punct_regex = re.compile(r'\w+|[^\w\s]+')
@@ -80,12 +80,28 @@ class Play(object):
                 1.0 / self.character_line_count(character)
                 )
 
-    def reconstitute(self):
+    def get_lines(self, character=None):
+        if character is None:
+            return self.lines
+        else:
+            return self.character_index.get(character, [])
+
+    def words(self, character=None):
+        return ' '.join(' '.join(ln.words) for ln in self.get_lines(character))
+
+    def regular_text(self, character=None):
+        return self._reconstitute(character, line_attr='words', join=' '.join)
+
+    def text(self, character=None):
+        return self._reconstitute(character)
+
+    def _reconstitute(self, character=None, line_attr='text', join='\n'.join):
+        lines = self.get_lines(character)
         fulltext = []
-        for line in self.lines:
+        for line in lines:
             fulltext.append(line.character)
-            fulltext.append(line.text)
-        return '\n'.join(fulltext)
+            fulltext.append(join(getattr(line, line_attr)))
+        return join(fulltext)
 
 def segment(text_lines, character_split):
     lines = []
@@ -147,29 +163,25 @@ class CharacterWordCount(object):
         for row in rows[1:]:
             print rowtemplate.format(row=row, maxlen=maxlen)
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Count words in a play.')
+def bag_of_ngrams(words, n=2):
+    iters = [iter(words) for _ in range(n)]
+    for n, it in enumerate(iters):
+        for _ in range(n):
+            next(it, None)
+    return Counter(zip(*iters))
 
-    sub = parser.add_subparsers()
-    cwords = sub.add_parser(
-        'count',
-        help='Count the number of words a character speaks in the given play.'
-    )
-    cwords.add_argument('text', type=str, help='The play text file.')
-    cwords.add_argument(
-        '--character', '-c', type=str, help='The name of the character.',
-        default=None
-    )
+def norm(A, B=None):
+    if B is None:
+        B = [0 for _ in A]
+    diff = [a - b for a, b in zip(A, B)]
+    return dot(diff, diff) ** 0.5
 
-    diff = sub.add_parser(
-        'diff', help='Return the approximate degree of difference between'
-        'two plays, at the character level, as a decimal fraction between'
-        '0.0 and 1.0.'
-    )
-    diff.add_argument('text_one', type=str, help='The first play text file.')
-    diff.add_argument('text_two', type=str, help='The second play text file.')
+def dot(A, B):
+    return sum(a * b for a, b in zip(A, B))
 
-    return parser.parse_args()
+def cosine_sim(A, B):
+    n = norm(A) * norm(B)
+    return norm if norm == 0 else dot(A, B) * 1.0 / n
 
 class Commands(object):
     def __init__(self):
@@ -198,7 +210,23 @@ class Commands(object):
         diff.add_argument(
             'text_two', type=str, help='The second play text file.'
         )
+        diff.add_argument(
+            '--character', '-c', type=str, help='The name of the character.',
+            default=None
+        )
+
         diff.set_defaults(command=self.diff)
+
+        bobsim = sub.add_parser(
+            'bobsim', help='Foo!!!'
+        )
+        bobsim.add_argument(
+            'text_one', type=str, help='The first play text file.'
+        )
+        bobsim.add_argument(
+            'text_two', type=str, help='The second play text file.'
+        )
+        bobsim.set_defaults(command=self.bobsim)
 
         self.args = parser.parse_args()
 
@@ -212,12 +240,25 @@ class Commands(object):
         char = self.args.character
         wc(None if char == 'all' else char)
 
-    def diff(self, *args, **kwargs):
-        a = self.args.text_one
-        b = self.args.text_two
+    def diff(self):
+        char = self.args.character
+        path_a = self.args.text_one
+        path_b = self.args.text_two
+        lines_a = loadlines(path_a, character_allcaps_split)
+        lines_b = loadlines(path_b, character_allcaps_split)
+        play_a = Play(lines_a)
+        play_b = Play(lines_b)
+        words_a = play_a.regular_text(char)
+        words_b = play_b.regular_text(char)
+        # print words_a
+        # print "__________________________________________"
+        # print words_b
+        s = SequenceMatcher(a=words_a,
+                            b=words_b)
 
-        s = SequenceMatcher(a=a, b=b)
-        print 'Ratio of difference betwteen the texts:'
+        msg = 'Ratio of similarity betwteen {} and {}'
+        msg += ':' if char is None else ' for character {}:'.format(char)
+        print msg.format(path_a, path_b)
         print '  {}'.format(s.ratio())
         print
         print '(Where T is the total number of elements in both sequences, '
@@ -225,6 +266,97 @@ class Commands(object):
         print ' that this is 1.0 if the sequences are identical, and 0.0 '
         print ' if they have nothing in common. '
         print '                --Python difflib documentation)'
+        print
+
+        msg = {
+            'replace': '{n}. Replace\n\n{text_a}\n\n with \n\n{text_b}\n\n',
+            'insert': '{n}. Insert\n\n{text_b}\n\n',
+            'delete': '{n}. Delete\n\n{text_a}\n\n',
+            'equal': '{n}. Preserve\n\n{text_a}\n\n',
+        }
+
+        for n, op in enumerate(s.get_opcodes()):
+            op, s1, e1, s2, e2 = op
+            print msg[op].format(n=n,
+                                 text_a=words_a[s1:e1],
+                                 text_b=words_b[s2:e2])
+
+    def bobsim(self):
+        path_a = self.args.text_one
+        path_b = self.args.text_two
+        lines_a = loadlines(path_a, character_allcaps_split)
+        lines_b = loadlines(path_b, character_allcaps_split)
+        lines_a = [l for l in lines_a if len(l.words) > 74]
+        lines_b = [l for l in lines_b if len(l.words) > 74]
+        bobs_a = [bag_of_ngrams(l.words) for l in lines_a]
+        bobs_b = [bag_of_ngrams(l.words) for l in lines_b]
+
+        vocab = set()
+        for bob in bobs_a:
+            vocab |= set(bob)
+
+        for bob in bobs_b:
+            vocab |= set(bob)
+
+        vectors_a = [[bob[bg] for bg in vocab] for bob in bobs_a]
+        vectors_b = [[bob[bg] for bg in vocab] for bob in bobs_b]
+        rows = []
+        for v in vectors_a:
+            row = [cosine_sim(v, u) for u in vectors_b]
+            rows.append(row)
+            for col in row:
+                print '{:0.1f}'.format(col),
+            print
+
+        for i, (line_a, row) in enumerate(zip(lines_a, rows)):
+            for j, csim in enumerate(row):
+                if csim > 0.1:
+                    line_b = lines_b[j]
+                    print "__________________________"
+                    print "Similar pair:"
+                    print
+                    print line_a.character
+                    print line_a.text
+                    print
+                    print line_b.character
+                    print line_b.text
+                    print
+                    print "____________"
+                    print "Edits:"
+                    df = Differ()
+                    for diffline in df.compare(line_a.text.splitlines(),
+                                               line_b.text.splitlines()):
+                        print diffline
+                    print
+                    print
+
+    def bobsim_sum(self):
+        """This was interesting because it picked out one line as especially
+        distinctive -- as especially similar to many other lines in the play.
+        Hunch: follows an exponential distribution; a few lines are very
+        similar to most lines and the value falls away quickly. Most
+        similar line is interesting somehow!
+        """
+        path = self.args.text
+        lines = loadlines(path, character_allcaps_split)
+        lines = [l for l in lines if len(l.words) > 74]
+        bobs = [bag_of_ngrams(l.words) for l in lines]
+
+        vocab = set()
+        for bob in bobs:
+            vocab |= set(bob)
+
+        vectors = [[bob[bg] for bg in vocab] for bob in bobs]
+        rowsums = []
+        for v in vectors:
+            row = [cosine_sim(v, u) for u in vectors]
+            for col in row:
+                print '{:0.1f}'.format(col),
+            rowsums.append(sum(row))
+            print
+        print lines[max(range(len(rowsums)), key=rowsums.__getitem__)]
+
+
 
 if __name__ == '__main__':
     cmd = Commands()
